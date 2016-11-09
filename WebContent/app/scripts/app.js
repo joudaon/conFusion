b49388c858cdc5bea47bbb19366413483c7fcef5
Uda <jonuda77@gmail.com>
2016-11-09 20:56:27 +0100

    Angular form validation in contactus.html
    + added ng tags to contactus.html to control form validation
    + added 2 new controllers to app.js to control form validation

--- WebContent/app/scripts/app.js
+++ WebContent/app/scripts/app.js
@@ -1,73 +1,109 @@
 'use strict';
-angular.module('confusionApp', []).controller('MenuController', ['$scope' , function($scope) {
-  $scope.tab = 1;
-  $scope.filtText = '';
-  $scope.showDetails = false;
-  
-  $scope.dishes = [
-    {
-      name:'Uthapizza',
-      image: 'img/uthappizza.png',
-      category: 'mains',
-      label:'Hot',
-      price:'4.99',
-      description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-      comment: ''
-    },
-    
-    {
-      name:'Zucchipakoda',
-      image: 'img/zucchipakoda.png',
-      category: 'appetizer',
-      label:'',
-      price:'1.99',
-      description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-      comment: ''
-    },
-    
-    {
-      name:'Vadonut',
-      image: 'img/vadonut.png',
-      category: 'appetizer',
-      label:'New',
-      price:'1.99',
-      description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-      comment: ''
-    },
-    
-    {
-      name:'ElaiCheese Cake',
-      image: 'img/elaicheesecake.png',
-      category: 'dessert',
-      label:'',
-      price:'2.99',
-      description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-      comment: ''
-    }
-  ]; 
+angular.module('confusionApp', [])
 
-  $scope.select = function(setTab) {
-	  $scope.tab = setTab;
-    
-    if (setTab === 2) {
-    	$scope.filtText = "appetizer";
-    } 
-    else if (setTab === 3) {
-    	$scope.filtText = "mains";
-    }
-    else if (setTab === 4) {
-    	$scope.filtText = "dessert";
-    }
-    else {
-    	$scope.filtText = "";
-    }
-  };
-  
-  $scope.isSelected = function (checkTab) {
-    return ($scope.tab === checkTab);
-  };
-  
-  $scope.toggleDetails = function() {
-	    $scope.showDetails = !$scope.showDetails;
-	};
-}]);
+	//// First controller - Menu Controller
+	.controller('MenuController', ['$scope' , function($scope) {
+	  $scope.tab = 1;
+	  $scope.filtText = '';
+	  $scope.showDetails = false;
+	  
+	  $scope.dishes = [
+	    {
+	      name:'Uthapizza',
+	  image: 'img/uthappizza.png',
+	  category: 'mains',
+	  label:'Hot',
+	  price:'4.99',
+	  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+	  comment: ''
+	},
+	
+	{
+	  name:'Zucchipakoda',
+	  image: 'img/zucchipakoda.png',
+	  category: 'appetizer',
+	  label:'',
+	  price:'1.99',
+	  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+	  comment: ''
+	},
+	
+	{
+	  name:'Vadonut',
+	  image: 'img/vadonut.png',
+	  category: 'appetizer',
+	  label:'New',
+	  price:'1.99',
+	  description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+	  comment: ''
+	},
+	
+	{
+	  name:'ElaiCheese Cake',
+	  image: 'img/elaicheesecake.png',
+	  category: 'dessert',
+	  label:'',
+	  price:'2.99',
+	  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+	  comment: ''
+	    }
+	  ]; 
+	
+	  $scope.select = function(setTab) {
+		  $scope.tab = setTab;
+	    
+	    if (setTab === 2) {
+	    	$scope.filtText = "appetizer";
+	} 
+	else if (setTab === 3) {
+		$scope.filtText = "mains";
+	}
+	else if (setTab === 4) {
+		$scope.filtText = "dessert";
+	}
+	else {
+		$scope.filtText = "";
+	    }
+	  };
+	  
+	  $scope.isSelected = function (checkTab) {
+	    return ($scope.tab === checkTab);
+	  };
+	  
+	  $scope.toggleDetails = function() {
+		    $scope.showDetails = !$scope.showDetails;
+		};
+	}])
+
+	// Second controller - Contact Controller
+	.controller('ContactController', ['$scope', function($scope) {
+	
+	    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+	                       agree:false, email:"" };
+	    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+        $scope.channels = channels;
+        $scope.invalidChannelSelection = false;
+	}])
+	
+	// Third controller - Feedback Controller
+	.controller('FeedbackController', ['$scope', function($scope) {
+	
+		$scope.sendFeedback = function() {
+            console.log($scope.feedback);
+            if ($scope.feedback.agree && ($scope.feedback.mychannel == "")&& !$scope.feedback.mychannel) {
+            	 $scope.invalidChannelSelection = true;
+                 console.log('incorrect');
+             }
+             else {
+                 $scope.invalidChannelSelection = false;
+                 $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                    agree:false, email:"" };
+                 $scope.feedback.mychannel="";
+
+                 $scope.feedbackForm.$setPristine();
+                 console.log($scope.feedback);
+             }
+         };
+		
+	}]);
+
