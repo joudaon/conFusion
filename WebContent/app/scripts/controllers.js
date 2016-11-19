2b929efb5d2e6842e4ebd0e877aed41b656922f4
joudaon <jonuda77@gmail.com>
2016-11-19 19:42:55 +0100

    Assignment 3 done
    + added factory methods is service.js
    + added controllers methods in controllers.js
    + added html code in home.html and aboutus.html

--- WebContent/app/scripts/controllers.js
+++ WebContent/app/scripts/controllers.js
@@ -94,6 +94,16 @@ angular.module('confusionApp')
         }])
 
         // implement the IndexController and About Controller here
-
+        .controller ('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
+        	
+    			$scope.dish = menuFactory.getDish (0);
+    			$scope.promotion = menuFactory.getPromotion(0);
+    			$scope.specialist = corporateFactory.getLeader(3);
+    	}])
+    	
+    	.controller ('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory){
+    		
+    			$scope.leaders = corporateFactory.getLeaders();
+    	}])
 
 ;
