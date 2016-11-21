e4d71afddf360b7d75ff55ff336c36ceca20a86d
joudaon <jonuda77@gmail.com>
2016-11-21 22:08:34 +0100

    Error handling in client-server communication with $http
    + added error handling in controller.js
    + added html come to display errors

--- WebContent/app/scripts/controllers.js
+++ WebContent/app/scripts/controllers.js
@@ -7,12 +7,18 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
+            $scope.showMenu = false;
+            $scope.message = "Loading...";
 
             $scope.dishes= {};
             menuFactory.getDishes()
             .then(
                 function(response) {
                     $scope.dishes = response.data;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
                 }
             );
 
@@ -77,13 +83,18 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
         	$scope.dish = {};
-            menuFactory.getDish(parseInt($stateParams.id,10))
-			.then(
-			    function(response){
-			        $scope.dish = response.data;
-			        $scope.showDish=true;
-    }
-);
+            $scope.showDish = false;
+            $scope.message="Loading ...";
+                        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+            );
             
         }])
 
@@ -108,12 +119,17 @@ angular.module('confusionApp')
         .controller ('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
         	
 	        	$scope.dish = {};
+	            $scope.showDish = false;
+	            $scope.message="Loading ...";
 	
 	            menuFactory.getDish(0)
 	            .then(
 	                function(response){
 	                    $scope.dish = response.data;
 	                    $scope.showDish = true;
+	                },
+	                function(response) {
+	                    $scope.message = "Error: "+response.status + " " + response.statusText;
 	                }
 	            );
 	            
