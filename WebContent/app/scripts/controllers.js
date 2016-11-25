1caff537a7cc4db684f8cc9ea8103ce4695c9336
joudaon <jonuda77@gmail.com>
2016-11-26 00:20:39 +0100

    Error handling using $resource

--- WebContent/app/scripts/controllers.js
+++ WebContent/app/scripts/controllers.js
@@ -7,10 +7,17 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = true;
+            $scope.showMenu = false;
             $scope.message = "Loading...";
 
-            $scope.dishes = menuFactory.getDishes().query();
+            menuFactory.getDishes().query(
+                    function(response) {
+                        $scope.dishes = response;
+                        $scope.showMenu = true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                    });
 
                         
             $scope.select = function(setTab) {
@@ -73,27 +80,33 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
         	$scope.dish = {};
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message="Loading ...";
-            
-            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)})
+        		.$promise.then(
+                function(response){
+                    $scope.dish = response;
+                    $scope.showDish = true;
+                },
+                function(response) {
+                    $scope.message = "Error: "+response.status + " " + response.statusText;
+                }
+    			);
             
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
             
-            $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+            $scope.comments = {rating:5, comment:"", author:"", date:""};
             
             $scope.submitComment = function () {
-                
-                $scope.mycomment.date = new Date().toISOString();
-                console.log($scope.mycomment);
-                
-                $scope.dish.comments.push($scope.mycomment);
-                
+                $scope.comments.date = new Date().toISOString();
+                console.log($scope.comments);
+                $scope.dish.comments.push($scope.comments);
+
+                menuFactory.getDishes().update({id:$scope.dish.id},$scope.dish);
                 $scope.commentForm.$setPristine();
-                
-                $scope.mycomment = {rating:5, comment:"", author:"", date:""};
+                $scope.comments = {rating:5, comment:"", author:"", date:""};
             }
         }])
 
@@ -101,10 +114,18 @@ angular.module('confusionApp')
         .controller ('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
         	
 	        	$scope.leader = corporateFactory.getLeader(3);
-	            $scope.showDish = true;
-	            $scope.message="Loading ...";
-	
-	            $scope.dish = menuFactory.getDishes().get({id:0});
+	        	$scope.showDish = false;
+                $scope.message="Loading ...";
+                $scope.dish = menuFactory.getDishes().get({id:0})
+                .$promise.then(
+                    function(response){
+                        $scope.dish = response;
+                        $scope.showDish = true;
+                    },
+                    function(response) {
+                        $scope.message = "Error: "+response.status + " " + response.statusText;
+                    }
+                );
 	            
     			$scope.promotion = menuFactory.getPromotion(0);
     			$scope.specialist = corporateFactory.getLeader(3);
