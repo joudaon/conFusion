f26b8248dc593e01015f2dca5786a21a4092745a
joudaon <jonuda77@gmail.com>
2016-11-25 20:13:52 +0100

    Client-server communication using ngResource
    + added support for ngResource

--- WebContent/app/scripts/controllers.js
+++ WebContent/app/scripts/controllers.js
@@ -7,20 +7,10 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = false;
+            $scope.showMenu = true;
             $scope.message = "Loading...";
 
-            $scope.dishes= {};
-            menuFactory.getDishes()
-            .then(
-                function(response) {
-                    $scope.dishes = response.data;
-                    $scope.showMenu = true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
+            $scope.dishes = menuFactory.getDishes().query();
 
                         
             $scope.select = function(setTab) {
@@ -83,18 +73,10 @@ angular.module('confusionApp')
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
         	$scope.dish = {};
-            $scope.showDish = false;
+            $scope.showDish = true;
             $scope.message="Loading ...";
-                        menuFactory.getDish(parseInt($stateParams.id,10))
-            .then(
-                function(response){
-                    $scope.dish = response.data;
-                    $scope.showDish=true;
-                },
-                function(response) {
-                    $scope.message = "Error: "+response.status + " " + response.statusText;
-                }
-            );
+            
+            $scope.dish = menuFactory.getDishes().get({id:parseInt($stateParams.id,10)});
             
         }])
 
@@ -118,20 +100,11 @@ angular.module('confusionApp')
         // implement the IndexController and About Controller here
         .controller ('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory){
         	
-	        	$scope.dish = {};
-	            $scope.showDish = false;
+	        	$scope.leader = corporateFactory.getLeader(3);
+	            $scope.showDish = true;
 	            $scope.message="Loading ...";
 	
-	            menuFactory.getDish(0)
-	            .then(
-	                function(response){
-	                    $scope.dish = response.data;
-	                    $scope.showDish = true;
-	                },
-	                function(response) {
-	                    $scope.message = "Error: "+response.status + " " + response.statusText;
-	                }
-	            );
+	            $scope.dish = menuFactory.getDishes().get({id:0});
 	            
     			$scope.promotion = menuFactory.getPromotion(0);
     			$scope.specialist = corporateFactory.getLeader(3);
