91d0afb6792ea6793158bc1d9b1f74861c7bbc84
joudaon <jonuda77@gmail.com>
2016-12-15 11:28:08 +0100

    Using Ionic´s model and forms (1)
    + small bug fixed

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -11,7 +11,6 @@ angular.module('conFusion.controllers', [])
 
   // Form data for the login modal
   $scope.loginData = {};
-  $scope.reservation = {};
   
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
