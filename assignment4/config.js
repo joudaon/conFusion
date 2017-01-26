273e20e85632291a2f9730c830f7e0577808cfa2
joudaon <jonuda77@gmail.com>
2017-01-26 10:05:19 +0100

    Assignment 4 folder created
    + Previously the task was done in "rest-server-passport" folder

---
+++ assignment4/config.js
@@ -0,0 +1,10 @@
+//Configuration information
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1883211645273000',
+        clientSecret: '39bb212f7945752ab8399abd0201ba61',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }   
+}
